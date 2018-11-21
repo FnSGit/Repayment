c261c7e0c4a6e9712081a8d15936af41ac928449
FnSGit <2696421750@qq.com>
2018-11-21 23:31:07 +0800

    yizhi_fkxx obj

---
+++ generate/src/com/fs/generate/target/entity/YizhiFkxxObj.java
@@ -0,0 +1,150 @@
+package com.fs.generate.target.entity;
+
+import com.fs.entity.TaskEntity;
+
+public class YizhiFkxxObj extends TaskEntity {
+	private String orderno;
+	private String fkje;
+	private String lilv;
+	private String qixian;
+	private String fkrq;
+	private String hxfs;
+	private String fwflv;
+	private String qudfflv;
+	private String schkr;
+	private String fysqfs;
+	private String qdffsqfs;
+	private String kouxifs;
+	private String jiesfs;
+	private String extrdate;
+	private String speclpro;
+	private String shifcuoq;
+	private String extrmony;
+	private String plfzuhao;
+	public void setOrderno(String orderno){
+	   this.orderno = orderno;
+	}
+	public String getOrderno(){
+	   return orderno;
+	}
+
+	public void setFkje(String fkje){
+	   this.fkje = fkje;
+	}
+	public String getFkje(){
+	   return fkje;
+	}
+
+	public void setLilv(String lilv){
+	   this.lilv = lilv;
+	}
+	public String getLilv(){
+	   return lilv;
+	}
+
+	public void setQixian(String qixian){
+	   this.qixian = qixian;
+	}
+	public String getQixian(){
+	   return qixian;
+	}
+
+	public void setFkrq(String fkrq){
+	   this.fkrq = fkrq;
+	}
+	public String getFkrq(){
+	   return fkrq;
+	}
+
+	public void setHxfs(String hxfs){
+	   this.hxfs = hxfs;
+	}
+	public String getHxfs(){
+	   return hxfs;
+	}
+
+	public void setFwflv(String fwflv){
+	   this.fwflv = fwflv;
+	}
+	public String getFwflv(){
+	   return fwflv;
+	}
+
+	public void setQudfflv(String qudfflv){
+	   this.qudfflv = qudfflv;
+	}
+	public String getQudfflv(){
+	   return qudfflv;
+	}
+
+	public void setSchkr(String schkr){
+	   this.schkr = schkr;
+	}
+	public String getSchkr(){
+	   return schkr;
+	}
+
+	public void setFysqfs(String fysqfs){
+	   this.fysqfs = fysqfs;
+	}
+	public String getFysqfs(){
+	   return fysqfs;
+	}
+
+	public void setQdffsqfs(String qdffsqfs){
+	   this.qdffsqfs = qdffsqfs;
+	}
+	public String getQdffsqfs(){
+	   return qdffsqfs;
+	}
+
+	public void setKouxifs(String kouxifs){
+	   this.kouxifs = kouxifs;
+	}
+	public String getKouxifs(){
+	   return kouxifs;
+	}
+
+	public void setJiesfs(String jiesfs){
+	   this.jiesfs = jiesfs;
+	}
+	public String getJiesfs(){
+	   return jiesfs;
+	}
+
+	public void setExtrdate(String extrdate){
+	   this.extrdate = extrdate;
+	}
+	public String getExtrdate(){
+	   return extrdate;
+	}
+
+	public void setSpeclpro(String speclpro){
+	   this.speclpro = speclpro;
+	}
+	public String getSpeclpro(){
+	   return speclpro;
+	}
+
+	public void setShifcuoq(String shifcuoq){
+	   this.shifcuoq = shifcuoq;
+	}
+	public String getShifcuoq(){
+	   return shifcuoq;
+	}
+
+	public void setExtrmony(String extrmony){
+	   this.extrmony = extrmony;
+	}
+	public String getExtrmony(){
+	   return extrmony;
+	}
+
+	public void setPlfzuhao(String plfzuhao){
+	   this.plfzuhao = plfzuhao;
+	}
+	public String getPlfzuhao(){
+	   return plfzuhao;
+	}
+
+}
